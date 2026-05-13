bf1a128961f941956e84c49a287b0110cb686ced
Nicolas Dias <nicolasbhgamer@gmail.com>
2026-05-13 12:31:53 -0300

    Fix errors and implement tests

--- src/r_loss_grad.c
+++ src/r_loss_grad.c
@@ -2,76 +2,110 @@
 
 void r_cross_entropy_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, RMatrix *grad)
 {
-    float norm = (float)pred->rows;
+    const float norm = (float)pred->rows;
 
     for (size_t i = 0; i < pred->rows; i++)
     {
         for (size_t j = 0; j < pred->cols; j++)
         {
-            float p = pred->data[RMatrixIDX(i, j, pred->cols)];
-            float y = real->data[RMatrixIDX(i, j, real->cols)];
-            grad->data[RMatrixIDX(i, j, grad->cols)] = -y / (norm * (p + EPSILON));
-        }
-    }
-}
+            const size_t idx_p = RMatrixIDX(i, j, pred->cols);
+            const size_t idx_y = RMatrixIDX(i, j, real->cols);
+            const size_t idx_g = RMatrixIDX(i, j, grad->cols);
 
-void r_bin_cross_entropy_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, RMatrix *grad)
-{
-    float norm = (float)(pred->rows * pred->cols);
+            const float p = pred->data[idx_p];
+            const float y = real->data[idx_y];
 
-    for (size_t i = 0; i < pred->rows; i++)
-    {
-        for (size_t j = 0; j < pred->cols; j++)
-        {
-            float p = pred->data[RMatrixIDX(i, j, pred->cols)];
-            float y = real->data[RMatrixIDX(i, j, real->cols)];
-            grad->data[RMatrixIDX(i, j, grad->cols)] =
-                (-(y / (p + EPSILON)) + ((1.0f - y) / (1.0f - p + EPSILON))) / norm;
+            grad->data[idx_g] = -y / (norm * (p + EPSILON));
         }
     }
 }
 
 void r_cat_cross_entropy_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, RMatrix *grad)
 {
-    float norm = (float)pred->rows;
+    const float norm = (float)pred->rows;
 
     for (size_t i = 0; i < pred->rows; i++)
     {
         for (size_t j = 0; j < pred->cols; j++)
         {
-            float p = pred->data[RMatrixIDX(i, j, pred->cols)];
-            float y = real->data[RMatrixIDX(i, j, real->cols)];
-            grad->data[RMatrixIDX(i, j, grad->cols)] = -y / (norm * (p + EPSILON));
+            const size_t idx_p = RMatrixIDX(i, j, pred->cols);
+            const size_t idx_y = RMatrixIDX(i, j, real->cols);
+            const size_t idx_g = RMatrixIDX(i, j, grad->cols);
+
+            const float p = pred->data[idx_p];
+            const float y = real->data[idx_y];
+
+            grad->data[idx_g] = -y / (norm * (p + EPSILON));
+        }
+    }
+}
+
+void r_bin_cross_entropy_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, RMatrix *grad)
+{
+    const float norm = (float)(pred->rows * pred->cols);
+
+    for (size_t i = 0; i < pred->rows; i++)
+    {
+        for (size_t j = 0; j < pred->cols; j++)
+        {
+            const size_t idx_p = RMatrixIDX(i, j, pred->cols);
+            const size_t idx_y = RMatrixIDX(i, j, real->cols);
+            const size_t idx_g = RMatrixIDX(i, j, grad->cols);
+
+            const float p = pred->data[idx_p];
+            const float y = real->data[idx_y];
+
+            const float num_pos = -y / (p + EPSILON);
+            const float num_neg = (1.0f - y) / (1.0f - p + EPSILON);
+
+            grad->data[idx_g] = (num_pos + num_neg) / norm;
         }
     }
 }
 
 void r_mse_loss_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, RMatrix *grad)
 {
-    float norm = (float)(pred->rows * pred->cols);
+    const float norm = (float)(pred->rows * pred->cols);
+    const float scale = 2.0f / norm;
 
     for (size_t i = 0; i < pred->rows; i++)
     {
         for (size_t j = 0; j < pred->cols; j++)
         {
-            float p = pred->data[RMatrixIDX(i, j, pred->cols)];
-            float y = real->data[RMatrixIDX(i, j, real->cols)];
-            grad->data[RMatrixIDX(i, j, grad->cols)] = 2.0f * (p - y) / norm;
+            const size_t idx_p = RMatrixIDX(i, j, pred->cols);
+            const size_t idx_y = RMatrixIDX(i, j, real->cols);
+            const size_t idx_g = RMatrixIDX(i, j, grad->cols);
+
+            const float p = pred->data[idx_p];
+            const float y = real->data[idx_y];
+
+            grad->data[idx_g] = scale * (p - y);
         }
     }
 }
 
 void r_mae_loss_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, RMatrix *grad)
 {
-    float norm = (float)(pred->rows * pred->cols);
+    const float norm = (float)(pred->rows * pred->cols);
+    const float inv_norm = 1.0f / norm;
 
     for (size_t i = 0; i < pred->rows; i++)
     {
         for (size_t j = 0; j < pred->cols; j++)
         {
-            float diff = pred->data[RMatrixIDX(i, j, pred->cols)] - real->data[RMatrixIDX(i, j, real->cols)];
-            float sign = (diff > 0.0f) ? 1.0f : (diff < 0.0f) ? -1.0f : 0.0f;
-            grad->data[RMatrixIDX(i, j, grad->cols)] = sign / norm;
+            const size_t idx_p = RMatrixIDX(i, j, pred->cols);
+            const size_t idx_y = RMatrixIDX(i, j, real->cols);
+            const size_t idx_g = RMatrixIDX(i, j, grad->cols);
+
+            const float diff = pred->data[idx_p] - real->data[idx_y];
+
+            float sign = 0.0f;
+            if (diff > 0.0f)
+                sign = 1.0f;
+            else if (diff < 0.0f)
+                sign = -1.0f;
+
+            grad->data[idx_g] = sign * inv_norm;
         }
     }
 }
@@ -79,48 +113,68 @@ void r_mae_loss_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real,
 void r_bin_focal_loss_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, float gamma, float alpha,
                            RMatrix *grad)
 {
-    float norm = (float)(pred->rows * pred->cols);
+    const float norm = (float)(pred->rows * pred->cols);
+    const float inv_norm = 1.0f / norm;
 
     for (size_t i = 0; i < pred->rows; i++)
     {
         for (size_t j = 0; j < pred->cols; j++)
         {
-            float p = pred->data[RMatrixIDX(i, j, pred->cols)];
-            float y = real->data[RMatrixIDX(i, j, real->cols)];
-            float omp = 1.0f - p;
-            float lp = logf(p + EPSILON);
-            float lop = logf(omp + EPSILON);
+            const size_t idx_p = RMatrixIDX(i, j, pred->cols);
+            const size_t idx_y = RMatrixIDX(i, j, real->cols);
+            const size_t idx_g = RMatrixIDX(i, j, grad->cols);
 
-            float d_t1 = alpha * y * (-gamma * powf(omp, gamma - 1.0f) * lp + powf(omp, gamma) / (p + EPSILON));
+            const float p = pred->data[idx_p];
+            const float y = real->data[idx_y];
 
-            float d_t2 =
-                (1.0f - alpha) * (1.0f - y) * (gamma * powf(p, gamma - 1.0f) * lop - powf(p, gamma) / (omp + EPSILON));
+            const float omp = 1.0f - p;
+            const float lp = logf(p + EPSILON);
+            const float lop = logf(omp + EPSILON);
 
-            grad->data[RMatrixIDX(i, j, grad->cols)] = -(d_t1 + d_t2) / norm;
+            const float omp_pow_gm1 = powf(omp, gamma - 1.0f);
+            const float omp_pow_g = omp_pow_gm1 * omp;
+
+            const float p_pow_gm1 = powf(p, gamma - 1.0f);
+            const float p_pow_g = p_pow_gm1 * p;
+
+            const float d_t1 = alpha * y * (-gamma * omp_pow_gm1 * lp + omp_pow_g / (p + EPSILON));
+
+            const float d_t2 = (1.0f - alpha) * (1.0f - y) * (gamma * p_pow_gm1 * lop - p_pow_g / (omp + EPSILON));
+
+            grad->data[idx_g] = -(d_t1 + d_t2) * inv_norm;
         }
     }
 }
 
 void r_cat_focal_loss_grad(const RNONNULL RMatrix *pred, const RNONNULL RMatrix *real, float gamma, RMatrix *grad)
 {
-    float norm = (float)pred->rows;
+    const float norm = (float)pred->rows;
+    const float inv_norm = 1.0f / norm;
 
     for (size_t i = 0; i < pred->rows; i++)
     {
         for (size_t j = 0; j < pred->cols; j++)
         {
-            float y = real->data[RMatrixIDX(i, j, real->cols)];
-            float g = 0.0f;
+            const size_t idx_p = RMatrixIDX(i, j, pred->cols);
+            const size_t idx_y = RMatrixIDX(i, j, real->cols);
+            const size_t idx_g = RMatrixIDX(i, j, grad->cols);
 
+            const float y = real->data[idx_y];
+
+            float g = 0.0f;
             if (y > 0.0f)
             {
-                float p = pred->data[RMatrixIDX(i, j, pred->cols)];
-                float omp = 1.0f - p;
-                g = -(y / norm) *
-                    (-gamma * powf(omp, gamma - 1.0f) * logf(p + EPSILON) + powf(omp, gamma) / (p + EPSILON));
+                const float p = pred->data[idx_p];
+                const float omp = 1.0f - p;
+
+                const float lp = logf(p + EPSILON);
+                const float omp_pow_gm1 = powf(omp, gamma - 1.0f);
+                const float omp_pow_g = omp_pow_gm1 * omp;
+
+                g = -y * inv_norm * (-gamma * omp_pow_gm1 * lp + omp_pow_g / (p + EPSILON));
             }
 
-            grad->data[RMatrixIDX(i, j, grad->cols)] = g;
+            grad->data[idx_g] = g;
         }
     }
 }
